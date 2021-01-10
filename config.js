390496153318f88f09595b6d7c4963575a818186
HarshVajani <70755020+HarshVajani@users.noreply.github.com>
2021-01-10 19:33:49 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCJnVJd8gD1lDO0vNRzF8yyro9V1RS3HeE",
-    authDomain: "wireless-buzzer-app-4f5b3.firebaseapp.com",
-    databaseURL: "https://wireless-buzzer-app-4f5b3-default-rtdb.firebaseio.com",
-    projectId: "wireless-buzzer-app-4f5b3",
-    storageBucket: "wireless-buzzer-app-4f5b3.appspot.com",
-    messagingSenderId: "388836574847",
-    appId: "1:388836574847:web:ff2a75e9e8d3f6d4d8a14a",
-    measurementId: "G-7SY434ZGNL"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyD8pb84yMYnHMTrTUzgTpXTi7WaE0DOVwo",
+  authDomain: "newsletter-2517e.firebaseapp.com",
+  databaseURL: "https://newsletter-2517e-default-rtdb.firebaseio.com",
+  projectId: "newsletter-2517e",
+  storageBucket: "newsletter-2517e.appspot.com",
+  messagingSenderId: "888066608609",
+  appId: "1:888066608609:web:27a0385b827860453596a0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
   export default firebase.database();
